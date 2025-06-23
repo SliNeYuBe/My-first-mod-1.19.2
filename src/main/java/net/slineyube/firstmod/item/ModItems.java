f8e2a5e78a8b5c21278054f06c99d282ae2b860b
Ilya Chukmaldin <ilya.chukmaldin@yandex.ru>
2025-06-24 00:27:17 +0500

    v0.4 Добавил кастомный предмет

--- src/main/java/net/slineyube/firstmod/item/ModItems.java
+++ src/main/java/net/slineyube/firstmod/item/ModItems.java
@@ -7,6 +7,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.slineyube.firstmod.TutorialMod;
+import net.slineyube.firstmod.item.custom.EightBallItem;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
@@ -17,6 +18,9 @@ public class ModItems {
 
     public static final RegistryObject<Item> RAW_ZIRCON = ITEMS.register("raw_zircon",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.MOD_TAB_2)));
+
+    public static final RegistryObject<Item> EIGHT_BALL = ITEMS.register("eight_ball",
+            () -> new EightBallItem(new Item.Properties().tab(ModCreativeModeTab.MOD_TAB_2).stacksTo(1)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
