dda17d700be4af5968d2a49a1991cbcab3b89867
Ilya Chukmaldin <ilya.chukmaldin@yandex.ru>
2025-06-27 23:58:33 +0500

    v0.7 Добавил состояние предметов

--- src/main/java/net/slineyube/firstmod/block/ModBlocks.java
+++ src/main/java/net/slineyube/firstmod/block/ModBlocks.java
@@ -6,26 +6,38 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.DropExperienceBlock;
+import net.minecraft.world.level.block.FireBlock;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.material.MaterialColor;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.slineyube.firstmod.TutorialMod;
 import net.slineyube.firstmod.block.custom.JumpyBlock;
+import net.slineyube.firstmod.block.custom.ZirconLampBlock;
 import net.slineyube.firstmod.item.ModCreativeModeTab;
 import net.slineyube.firstmod.item.ModItems;
 
+import java.awt.*;
 import java.util.function.Supplier;
 
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, TutorialMod.MOD_ID);
 
+//    public static final RegistryObject<Block> ZIRCON_BLOCK = registerBlock("zircon_block",
+//            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
+//                    .strength(6f).requiresCorrectToolForDrops()), ModCreativeModeTab.MOD_TAB_1);
+
     public static final RegistryObject<Block> ZIRCON_BLOCK = registerBlock("zircon_block",
             () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
-                    .strength(6f).requiresCorrectToolForDrops()), ModCreativeModeTab.MOD_TAB_1);
+                    .color(MaterialColor.COLOR_BLUE)
+                    .sound(SoundType.AMETHYST)
+                    .strength(6f)
+                    .requiresCorrectToolForDrops()), ModCreativeModeTab.MOD_TAB_1);
 
     public static final RegistryObject<Block> ZIRCON_ORE = registerBlock("zircon_ore",
             () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE)
@@ -51,6 +63,11 @@ public class ModBlocks {
             () -> new JumpyBlock(BlockBehaviour.Properties.of(Material.STONE)
                     .strength(6f).requiresCorrectToolForDrops(),
                     UniformInt.of(100000,200000)), ModCreativeModeTab.MOD_TAB_1);
+
+    public static final RegistryObject<Block> ZIRCON_LAMP = registerBlock("zircon_lamp",
+            () -> new ZirconLampBlock(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(6f).requiresCorrectToolForDrops()
+                    .lightLevel(state -> state.getValue(ZirconLampBlock.LIT) ? 15 : 0)), ModCreativeModeTab.MOD_TAB_1);
 
 
     private static <T extends Block> RegistryObject<T> registerBlock(final String name, final Supplier<T> block, CreativeModeTab tab) {
---
+++ src/main/java/net/slineyube/firstmod/block/custom/ZirconLampBlock.java
@@ -0,0 +1,47 @@
+package net.slineyube.firstmod.block.custom;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.phys.BlockHitResult;
+
+public class ZirconLampBlock extends Block {
+    public static final BooleanProperty LIT = BlockStateProperties.LIT;
+
+    public ZirconLampBlock(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public InteractionResult use(BlockState state, Level level, BlockPos blockPos, Player player, InteractionHand hand, BlockHitResult result) {
+        if (!level.isClientSide() && hand == InteractionHand.MAIN_HAND) {
+            level.setBlock(blockPos, state.cycle(LIT), 3);
+        }
+
+        return super.use(state, level, blockPos, player, hand, result);
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(LIT);
+    }
+
+    @Override
+    public int getFlammability(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+        return 5;
+    }
+
+    @Override
+    public int getFireSpreadSpeed(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+        return 20;
+    }
+}
